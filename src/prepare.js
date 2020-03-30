2e5cd2f29cf53ef88aee7c14f7fad19e179324f9
Ola Flisbäck <ola.flisback@gmail.com>
2020-03-30 20:23:17 +0200

    Prepared restructuring for deaths since day 0

--- src/prepare.js
+++ src/prepare.js
@@ -1,10 +1,9 @@
 const data = require("./response.js").data;
+const moment = require("moment");
 
 console.log("Nbr results: " + data.results.length);
 
 let dateToValueMap = {};
-
-console.log(data.results[0]);
 
 data.results.forEach((entry) => {
   if (dateToValueMap.hasOwnProperty(entry.date)) {
@@ -37,16 +36,59 @@ Object.entries(dateToValueMap).forEach(([date, entry]) => {
 
 console.log(graphData);
 
-/*
-const receivedData = data.getStuff.results.map(
-    (value: { country: { name: string }; date: string; deaths: number }) => ({
-      country: value.country.name,
-      date: value.date,
-      deaths: value.deaths,
-    })
+const countries = ["Italy", "US"];
+
+let countryToDayZeroMap = {};
+
+const DAY_ZERO_DEATHS = 1000;
+const DATE_FORMAT = "YYYY-M-D";
+
+countries.forEach((country) => {
+  let minimumDayZeroDate = moment();
+  Object.entries(dateToValueMap).forEach(([date, entry]) => {
+    if (entry[country].deaths > DAY_ZERO_DEATHS) {
+      if (!countryToDayZeroMap[country]) {
+        countryToDayZeroMap[country] = date;
+        minimumDayZeroDate = moment(date, DATE_FORMAT);
+      } else {
+        if (minimumDayZeroDate.isAfter(moment(date, DATE_FORMAT))) {
+          countryToDayZeroMap[country] = date;
+          minimumDayZeroDate = moment(date, DATE_FORMAT);
+        }
+      }
+    }
+  });
+});
+
+const minDayZero = Object.values(countryToDayZeroMap).reduce((a, b) =>
+  moment.min([moment(a, DATE_FORMAT), moment(b, DATE_FORMAT)])
+);
+
+const lastDay = Object.keys(dateToValueMap).reduce((a, b) =>
+  moment.max([moment(a, DATE_FORMAT), moment(b, DATE_FORMAT)])
+);
+
+console.log("First dayzero: " + minDayZero.format(DATE_FORMAT));
+
+const nbrOfDays = moment.duration(lastDay.diff(minDayZero)).asDays() + 1;
+
+let dayZeroGraphData = new Array(nbrOfDays).fill({});
+
+countries.forEach((country) => {
+  const startIndex = Object.keys(dateToValueMap).findIndex(
+    (date) => date === countryToDayZeroMap[country]
   );
 
+  let insertIndex = 0;
+  const dates = Object.keys(dateToValueMap);
+  for (let i = startIndex; i < dates.length; i++) {
+    dayZeroGraphData[insertIndex] = {
+      ...dayZeroGraphData[insertIndex],
+      day: insertIndex,
+      [country]: dateToValueMap[dates[i]][country].deaths,
+    };
+    insertIndex++;
+  }
+});
 
-
-console.log(data);
-*/
+console.log(dayZeroGraphData);
