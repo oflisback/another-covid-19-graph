bd6f3a484fc763342b5443ee8915da45a963ef5f
Ola Flisbäck <ola.flisback@gmail.com>
2020-04-04 12:50:25 +0200

    Separate static from query with yarn script

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,11 @@
-import Graph from "containers/GraphWithQuery";
+import GraphWithQuery from "containers/GraphWithQuery";
+import GraphWithStatic from "containers/GraphWithStatic";
 import React from "react";
 import SettingsPanel from "containers/SettingsPanel";
 
 export default () => (
   <>
     <SettingsPanel />
-    <Graph />
+    {process.env.NODE_ENV === "production" || process.env.REACT_APP_STATIC_DATA === 'false' ? <GraphWithQuery /> : <GraphWithStatic />}
   </>
-);
+)
