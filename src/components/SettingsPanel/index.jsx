e91607c8fb88af03d61f61c86ff391de3c6ff707
Ola Flisbäck <ola.flisback@gmail.com>
2020-04-05 14:28:24 +0200

    Add github link

--- src/components/SettingsPanel/index.jsx
+++ src/components/SettingsPanel/index.jsx
@@ -25,7 +25,10 @@ const SettingsPanel = ({ countries, options, toggleCountryEnabled, toggleCumulat
         backgroundColor: "white",
         padding: "15px",
         maxWidth: '200px',
-        width: '200px'
+        width: '200px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
     }}>
       <FormControl component="fieldset" className={classes.formControl}>
         <FormLabel component="legend">Countries</FormLabel>
@@ -70,7 +73,10 @@ const SettingsPanel = ({ countries, options, toggleCountryEnabled, toggleCumulat
           />
         </FormGroup>
       </FormControl>
-    Data from <a href="https://systems.jhu.edu/research/public-health/ncov/">John Hopkins University</a> via this <a href="https://github.com/rlindskog/covid19-graphql">GraphQL API</a>.
+      <div>Data from <a href="https://systems.jhu.edu/research/public-health/ncov/">John Hopkins University</a> via this <a href="https://github.com/rlindskog/covid19-graphql">GraphQL API</a>.</div>
+      <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', marginBottom: '20px', marginTop: 'auto'}}>
+        <img src="github.png" alt="github" style={{ width: '32px', height: '32px' }} onClick={() => window.open("https://github.com/oflisback/another-covid-19-graph", "_blank")} />
+      </div>
     </div>
   );
 };
